6c8caf891436fda53179bbbd04d74fd63d7f4db5
Naoto Yamaguchi <naotoy@hawaii.edu>
2017-02-18 09:30:01 -1000

    made initial tests for basic passes, need to work on unexpected inputss and invalid arguments

--- test/throwing-spec.js
+++ test/throwing-spec.js
@@ -2,14 +2,22 @@ let chai = require('chai');
 let scoreThrows = require('../index.js');
 chai.should();
 
-describe('Throwing darts test', function() {
-  var throwTest;
-  beforeEach(function() {
-    console.log(scoreThrows([1, 5, 11]));
+describe('Throwing dart score', function() {
+
+  it('should be an number', function() {
+    scoreThrows([1, 2, 3, 4]).should.be.a('number');
   });
 
-  it('should be an array', function() {
-    throwTest.should.be.an('array');
+  it('should return score 140' , function() {
+    scoreThrows([1, 2, 3, 4]).should.equal(140);
+  });
+
+  it('should return a score of 0', function() {
+    scoreThrows([15, 20, 30]).should.equal(0);
+  });
+
+  it('should return a score of 15', function() {
+    scoreThrows([1, 5, 11]).should.equal(15);
   });
 
 
